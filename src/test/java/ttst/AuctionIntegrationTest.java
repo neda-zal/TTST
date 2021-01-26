8dbaabf9908fa6270de051b6df4892ebd0cf4d84
Filippi Davide (Student Com17) <davide.filippi@stud-inf.unibz.it>
2021-01-27 00:33:29 +0100

    Update AuctionIntegrationTest.java

--- src/test/java/ttst/AuctionIntegrationTest.java
+++ src/test/java/ttst/AuctionIntegrationTest.java
@@ -1,3 +1,5 @@
+package ttst;
+
 import org.junit.Test;
 import ttst.Auction;
 import ttst.Item;
@@ -10,32 +12,27 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class auctionIntegrationTest {
+public class auctionIntegrationTestUnits {
 
     //test for the searchHighestBid() function in the .jar
-    private static Date today = Calendar.getInstance().getTime();
+    private static Calendar calendar = Calendar.getInstance();
+    private static Date today = calendar.getTime();
+
     private User user = new User("test","test");
+    Item item = new Item("description test",50.00,null,3);
 
     @Test
     public void searchHighestBidTest() {
         List<Bid> bids = new ArrayList<Bid>();
+        Auction auction = new Auction(item, 250.00,23000000,23500000);
         //bids.add(12.50,today,user);
         Bid max = null;
         String msg = null;
-        try{
-            Auction.searchHighestBid(bids);
-        } catch (Exception e) {
-            msg = e.toString();
-        }
-        assertNull(msg, bids);
+
+        assertNull("the list of bids is empty", auction.searchHighestBid(bids));
 
         //bids.add(12.50,today,user);
-        try{
-            Auction.searchHighestBid(bids);
-        } catch (Exception e){
-            msg = e.toString();
-        }
-        assertNotNull(msg, max);
+        //assertNotNull("The highest bid", max);
     }
 
 }
