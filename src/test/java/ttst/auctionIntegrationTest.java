ceb2935d07abb2d595cb875e4ddcfc174656720e
Filippi Davide (Student Com17) <davide.filippi@stud-inf.unibz.it>
2021-01-26 23:01:52 +0100

    Creating the searchHighestBid Unit Test

---
+++ src/test/java/ttst/auctionIntegrationTest.java
@@ -0,0 +1,35 @@
+package ttst;
+
+import org.junit.Test;
+import ttst.Auction;
+import ttst.Item;
+import ttst.Bid;
+import ttst.User;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class auctionIntegrationTestUnits {
+
+    //test for the searchHighestBid() function in the .jar
+    private static Date today = Calendar.getInstance().getTime();
+    private User user = new User("test","test");
+
+    @Test
+    public void searchHighestBidTest() {
+        List<Bid> bids = new ArrayList<Bid>();
+        //bids.add(12.50,today,user);
+        String msg;
+        try{
+            searchHighestBid(bids);
+        } catch (Exception e) {
+            msg = e.toString();
+        }
+
+        assertNotNull(msg, bids);
+    }
+
+}
